bf06a9e363bb5ca1da0dce505c93aab37d3ac5a8
Roman <romagilda@yandex.ru>
2025-03-06 12:53:44 +0300

    fix css GameInformation

--- src/features/game-information/GameInformation.tsx
+++ src/features/game-information/GameInformation.tsx
@@ -13,13 +13,13 @@ export function GameInformation({details}: GameInformationProps) {
         <img className={css.picture} src={details.background_image} alt="картинка с игрой" />
         <div className={css.block_game_information}>
           <ul className={css.game_information}>
-            <li className={css.information}>Дата обновления: <span className='style_word'>{details.updated}</span></li>
+            <li className={css.information}>Дата обновления: <span className={css.style_word}>{details.updated}</span></li>
             <li className={css.information}>Описание:</li>
             <li className={css.style_word} dangerouslySetInnerHTML={{ __html: details.description }}></li>
-            <li className={css.information}>Официальный сайт: <a className='style_web' href={details.website} target="_blank">{details.website}</a></li>
-            <li className={css.information}>Обновление: <span className='style_word'>{details.updated}</span></li>
-            <li className={css.information}>Релиз: <span className='style_word'>{details.released}</span></li>
-            <li className={css.information}>Время игры: <span className='style_word'>{details.playtime} h</span> </li>
+            <li className={css.information}>Официальный сайт: <a className={css.style_web} href={details.website} target="_blank">{details.website}</a></li>
+            <li className={css.information}>Обновление: <span className={css.style_word}>{details.updated}</span></li>
+            <li className={css.information}>Релиз: <span className={css.style_word}>{details.released}</span></li>
+            <li className={css.information}>Время игры: <span className={css.style_word}>{details.playtime} h</span> </li>
           </ul>
         </div>  
       </div>
