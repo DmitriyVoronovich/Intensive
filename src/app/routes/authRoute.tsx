e7c1260fb40592dcc35d70d77e33d072701f86e8
Dmitriy Vorornovich <dmitriy.vr20@gmail.com>
2025-03-12 09:48:47 +0300

    feat/INT-21: Added protection for the "Favorites" and "Search History" pages

--- src/app/routes/authRoute.tsx
+++ src/app/routes/authRoute.tsx
@@ -1,8 +1,19 @@
-import { BaseLayout } from '../../shared';
-import { useOutlet } from 'react-router-dom';
+import {BaseLayout, getUserFromLS, PATHS} from '../../shared';
+import {Navigate, useOutlet} from 'react-router-dom';
 import { Header } from '../../shared';
 
-export const AuthRoute = () => {
+type Props = {
+  isProtected?: boolean
+}
+
+export const AuthRoute = ({isProtected}: Props) => {
   const outlet = useOutlet();
+
+  const user = getUserFromLS();
+
+  if (!user && isProtected) {
+    return <Navigate to={PATHS.SINGNIN} />
+  }
+
   return <BaseLayout header={<Header />} outlet={outlet} />;
 };
